a20fa96e02a5eb029a30fecac995feaec46c1969
victorpellapore-wq <victorpellapore@gmail.com>
2026-09-21 12:52:17 -0300

    scooby

--- Casinha.py
+++ Casinha.py
@@ -5,7 +5,7 @@ init()
 screen = display.set_mode((800,600))
 
 #recursos
-bat_imagem = image.load("batman.png")
+bat_imagem = image.load("scooby")
 bat_imagem = transform.scale(bat_imagem, (200,200))
 bat_fonte= font.Font("AlienBlock-Regular.ttf", 50)
 mixer.music.load("batman_1966.mp3")
